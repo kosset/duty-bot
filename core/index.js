599fa60758541de39e3234c89757473a797fcea8
Kostas Setzas <setkostas@gmail.com>
2018-10-06 00:11:10 +0300

    Architecture refinements

---
+++ core/index.js
@@ -0,0 +1,21 @@
+const event = require('./converters');
+
+module.exports = {
+
+  manageResponse: async function(res) {
+
+    //TODO: Convert Incoming Response to An Event
+
+    //TODO: Fetch from the DB the correct stored data
+
+    //TODO: Pre NLProcess (Some times we may skip it, e.g. on most Postbacks)
+
+    //TODO: NLProcess
+
+    //TODO: Post NLProcess
+
+    //TODO: Convert Response to PlatformResponse and Send the Response
+    //TODO: Store the needed Data
+  }
+
+};
--- index.js
+++ index.js
@@ -1,86 +1,40 @@
-#!/usr/bin/env node
-
 /**
- * Module dependencies.
+ * NPM Module Dependencies
  */
 const
-    app = require("./app"),
-    http = require("http"),
-    logger = require('./loggers').appLogger,
-    debug = require("debug")("hygieia:server");
-/**
- * Get port from environment and store in Express.
- */
-
-const port = normalizePort(process.env.PORT || "3000");
-app.set("port", port);
+  express = require("express"),
+  mongoose = require("mongoose"),
+  path = require("path"),
+  logger = require('./loggers').appLogger,
+  cookieParser = require("cookie-parser"),
+  config = require('config');
 
 /**
- * Create HTTP server.
+ * LOCAL Module Dependencies
  */
+const
+  indexRouter = require('./routes/index'),
+  apiRouter = require('./routes/api');
 
-const server = http.createServer(app);
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
 
 /**
- * Listen on provided port, on all network interfaces.
+ * Endpoints
  */
-
-server.listen(port);
-server.on("error", onError);
-server.on("listening", onListening);
+app.use('/', indexRouter);
+app.use(['/api','/api/'], apiRouter);
 
 /**
- * Normalize a port into a number, string, or false.
+ * Database Connection
  */
+mongoose.connect(config.get('mongo.url'), {useNewUrlParser: true}).then(
+  () => { logger.info(`Successful connection with MongoDB`)},
+  error => { logger.error(error); process.exit(1);}
+);
 
-function normalizePort(val) {
-  let port = parseInt(val, 10);
-
-  if (isNaN(port)) {
-    // named pipe
-    return val;
-  }
-
-  if (port >= 0) {
-    // port number
-    return port;
-  }
-
-  return false;
-}
-
-/**
- * Event listener for HTTP server "error" event.
- */
-
-function onError(error) {
-  if (error.syscall !== "listen") {
-    throw error;
-  }
-
-  const bind = typeof port === "string" ? "Pipe " + port : "Port " + port;
-
-  // handle specific listen errors with friendly messages
-  switch (error.code) {
-    case "EACCES":
-      logger.error(bind + " requires elevated privileges");
-      process.exit(1);
-      break;
-    case "EADDRINUSE":
-      logger.error(bind + " is already in use");
-      process.exit(1);
-      break;
-    default:
-      throw error;
-  }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-
-function onListening() {
-  const addr = server.address();
-  const bind = typeof addr === "string" ? "pipe " + addr : "port " + addr.port;
-  logger.info("Listening on " + bind);
-}
+module.exports = app;
