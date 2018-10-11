1c84d5d0940f853f69f5733b66ae0ead057c044b
Kostas Setzas <setkostas@gmail.com>
2018-10-11 23:59:28 +0300

    Built converter to fb response

--- core/converters/facebook.converter.js
+++ core/converters/facebook.converter.js
@@ -6,72 +6,99 @@ module.exports = class FacebookConverter {
   }
 
   toResponseMessage(event, userData, nlpResponse) {
-    const
-      that = this,
-      psid = userData.psid;
+    const psid = userData.psid;
 
     return nlpResponse.fulfillmentMessages.map((msg, index) => {
       if (!("platform" in msg) || msg.platform === "FACEBOOK") {
-        if ("text" in msg) {
-          // Text
-          return {
-            messaging_type: "RESPONSE",
-            recipient: {
-              id: psid
-            },
-            message: {
-              text: msg.text.text[0] //TODO: Handle this better
-            }
-          };
-        } else if ("image" in msg) {
-          // Image TODO: May be I should change this
-          return {
-            messaging_type: "RESPONSE",
-            recipient: {
-              id: psid
-            },
-            message: {
-              attachment: {
-                type: "template",
-                payload: {
-                  template_type: "media",
-                  elements: [
-                    {
-                      media_type: "image",
-                      attachment_id: msg.image.imageUrl
-                    }
-                  ]
+        switch (msg.type) {
+          case 0: //text
+            return {
+              messaging_type: "RESPONSE",
+              recipient: {
+                id: psid
+              },
+              message: {
+                text: msg.speech //TODO: Handle this better
+              }
+            };
+          case 1: // Card or Carousel
+            return {
+              recipient:{
+                id: psid
+              },
+              message:{
+                attachment:{
+                  type:"template",
+                  payload:{
+                    template_type:"generic",
+                    elements:[
+                      {
+                        title: msg.title || `Card's Title`,
+                        image_url: msg.imageUrl,
+                        subtitle: msg.subtitle,
+                        buttons: msg.buttons.map(btn => {
+                          if (btn.postback.startsWith('http')) {
+                            return {
+                              type: "web_url",
+                              url: btn.postback,
+                              title: btn.text
+                            };
+                          } else {
+                            return {
+                              type: "postback",
+                              title: btn.postback,
+                              payload: btn.postback
+                            };
+                          }
+                        })
+                      }
+                    ]
+                  }
                 }
               }
-            }
-          };
-        } else if ("quickReplies" in msg) {
-          // QuickReplies
-          return {
-            messaging_type: "RESPONSE",
-            recipient: {
-              id: psid
-            },
-            message: {
-              text: msg.quickReplies.title,
-              quick_replies: msg.quickReplies.quickReplies.map(qr => {
-                return {
-                  content_type: "text",
-                  title: qr.length > 20 ? "ERROR: Wrong Length" : qr,
-                  payload: qr
-                };
-              })
-            }
-          };
-        } else if ("card" in msg) {
-          // Card or Carousel
-          //TODO: Create a check here for that responses
-        } else {
-          throw new Error(
-            `That type of message has not been implemented for facebook yet: ${JSON.stringify(
-              msg
-            )}`
-          );
+            };
+          case 2: // Quick Replies
+            return {
+              messaging_type: "RESPONSE",
+              recipient: {
+                id: psid
+              },
+              message: {
+                text: msg.title,
+                quick_replies: msg.replies.map(qr => {
+                  return {
+                    content_type: "text",
+                    title: qr.length > 20 ? "ERROR: Wrong Length" : qr,
+                    payload: qr
+                  };
+                })
+              }
+            };
+          case 3: // Image //TODO: May be I should change this
+            return {
+              messaging_type: "RESPONSE",
+              recipient: {
+                id: psid
+              },
+              message: {
+                attachment: {
+                  type: "template",
+                  payload: {
+                    template_type: "media",
+                    elements: [
+                      {
+                        media_type: "image",
+                        attachment_id: msg.imageUrl
+                      }
+                    ]
+                  }
+                }
+              }
+            };
+          case 4: // Custom Payload
+            return msg;
+          default:
+            throw new Error(`No response implementation for that type of DF Response: ${JSON.stringify(msg)}`);
         }
       }
     });
